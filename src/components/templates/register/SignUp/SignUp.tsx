cada748015206445bb1c2cf9646b429a11624374
AshkanZj <ashkanzojaji19@gmail.com>
2024-07-13 15:08:14 +0330

    feat: complete signup logic

--- src/components/templates/register/SignUp/SignUp.tsx
+++ src/components/templates/register/SignUp/SignUp.tsx
@@ -12,39 +12,25 @@ import { NewUserType } from "@/src/types/types";
 import { setCookie } from "cookies-next";
 import { FieldValues, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { useSignup } from "@/src/api/auth/auth.queries";
+import { newUserType } from "@/src/api/auth/auth.type";
+import { TbEyeClosed } from "react-icons/tb";
+import { CgEye } from "react-icons/cg";
 
 export default function SignUpTemplate({ active }: { active: boolean }) {
-  const { register, handleSubmit } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { t } = useTranslation();
 
-  // const { push: pushRouter } = useRouter();
-  // const { state, dispatch } = useUserContext();
-
-  // useEffect(() => {
-  //   state.isLogin && pushRouter(MainRoutes.HOME);
-  // }, [state.isLogin]);
+  const { mutate: signupMutate } = useSignup();
 
   const handleSignUp = (data: FieldValues) => {
-    console.log(data);
-    //   if (name && email && password) {
-    //     const newUser: NewUserType = {
-    //       firstname: name,
-    //       lastname: "",
-    //       username: name,
-    //       password: password,
-    //       phoneNumber: "",
-    //       address: "",
-    //     };
-    //     postUserData(newUser).then(({ token, data: { user } }) => {
-    //       setCookie("username", user.username);
-    //       setCookie("accessToken", token.accessToken);
-    //       dispatch({
-    //         type: AuthReducerAction.LOGIN,
-    //         payload: { ...user, accessToken: token.accessToken },
-    //       });
-    //       pushRouter(MainRoutes.HOME);
-    //     });
-    //   } else toast.error("Please enter valid Name,Email and Password!");
+    const newUser = data as newUserType;
+    signupMutate(newUser);
   };
 
   return (
@@ -57,22 +43,7 @@ export default function SignUpTemplate({ active }: { active: boolean }) {
         className="flex flex-col justify-center items-center py-10 h-full"
         onSubmit={handleSubmit(handleSignUp)}
       >
-        <h3 className="font-bold text-3xl">{t("create-account")}</h3>
-        {/* login options */}
-        <div className="flex flex-row gap-3 my-4">
-          <MyIconBtn>
-            <FaGooglePlusG size={20} color="black" />
-          </MyIconBtn>
-          <MyIconBtn>
-            <FaFacebookF size={15} color="black" />
-          </MyIconBtn>
-          <MyIconBtn>
-            <FaGithub size={15} color="black" />
-          </MyIconBtn>
-          <MyIconBtn>
-            <FaLinkedinIn size={15} color="black" />
-          </MyIconBtn>
-        </div>
+        <h3 className="font-bold text-3xl mb-5">{t("create-account")}</h3>
         {/* inputs */}
         <p className="text-xs">{t("use-details-for-registration")}</p>
         <MyInput
@@ -81,44 +52,105 @@ export default function SignUpTemplate({ active }: { active: boolean }) {
           name="firstname"
           register={register}
           required={true}
+          pattern={/^[a-zA-Z].{4,}$/}
         />
+        {/* firstname error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.firstname ? "visible" : "invisible"
+          }`}
+        >
+          {t("firstname-input-error")}
+        </p>
         <MyInput
           type="text"
           placeholder={t("lastname")}
           name="lastname"
           register={register}
           required={true}
+          pattern={/^[a-zA-Z].{4,}$/}
         />
+        {/* lastname error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.lastname ? "visible" : "invisible"
+          }`}
+        >
+          {t("lastname-input-error")}
+        </p>
         <MyInput
           type="text"
           placeholder={t("phone-number")}
           name="phoneNumber"
           register={register}
           required={true}
+          pattern={/^\d{11}$/}
         />
+        {/* phone number error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.phoneNumber ? "visible" : "invisible"
+          }`}
+        >
+          {t("phone-number-input-error")}
+        </p>
         <MyInput
           type="text"
           placeholder={t("address")}
           name="address"
           register={register}
           required={true}
+          pattern={/^.{4,}$/}
         />
+        {/* address error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.address ? "visible" : "invisible"
+          }`}
+        >
+          {t("address-input-error")}
+        </p>
         <MyInput
           type="text"
           placeholder={t("username")}
           name="username"
           register={register}
           required={true}
+          pattern={/^.{4,15}$/}
         />
+        {/* username error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.username ? "visible" : "invisible"
+          }`}
+        >
+          {t("username-input-error")}
+        </p>
         <MyInput
-          type="password"
+          type={`${showPassword ? "text" : "password"}`}
           placeholder={t("password")}
           name="password"
           register={register}
           required={true}
+          pattern={/^(?=.*\d)(?=.*[a-z]).{6,}$/}
+          icon={
+            showPassword ? (
+              <CgEye onClick={() => setShowPassword(false)} />
+            ) : (
+              <TbEyeClosed onClick={() => setShowPassword(true)} />
+            )
+          }
         />
+        {/* password error message */}
+        <p
+          className={`text-rose-400 text-xs w-2/3 text-center ${
+            errors.password ? "visible" : "invisible"
+          }`}
+        >
+          {t("password-input-error")}
+        </p>
         <button
-          className="bg-axLightPurple text-white text-xs py-2 px-11 rounded-lg font-semibold tracking-wide uppercase mt-2 hover:bg-axDarkPurple"
+          className="bg-axLightPurple text-white text-xs py-4 px-14 rounded-lg font-semibold tracking-wide uppercase mt-2 hover:bg-axDarkPurple"
           type="submit"
         >
           {t("signup")}

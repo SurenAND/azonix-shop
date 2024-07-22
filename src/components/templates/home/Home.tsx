f5f1bef11c3c3611001ac607381e1232b4210727
AshkanZj <ashkanzojaji19@gmail.com>
2024-07-22 16:04:00 +0330

    feat: create categories section in home page

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,9 +1,11 @@
+import Categories from "@/src/components/templates/home/categories/Categories";
 import Hero from "@/src/components/templates/home/hero/Hero";
 
 export default function HomeTemplate() {
   return (
     <div>
       <Hero />
+      <Categories />
     </div>
   );
 }

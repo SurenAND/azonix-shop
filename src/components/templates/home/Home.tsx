b798f3e4a2984f6c7851a112da09d32641849ee5
AshkanZj <ashkanzojaji19@gmail.com>
2024-07-23 13:48:04 +0330

    feat: create recent news section

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -2,6 +2,7 @@ import BestSellerProducts from "@/src/components/templates/home/best-seller/Best
 import Categories from "@/src/components/templates/home/categories/Categories";
 import Hero from "@/src/components/templates/home/hero/Hero";
 import OnSaleBanner from "@/src/components/templates/home/on-sale/OnSaleBanner";
+import RecentNews from "@/src/components/templates/home/recent-news/RecentNews";
 import Services from "@/src/components/templates/home/services/Services";
 import { firstBannerData, secondBannerData } from "@/src/constant/bannerData";
 import { useEffect, useState } from "react";
@@ -22,6 +23,7 @@ export default function HomeTemplate() {
           <OnSaleBanner data={firstBannerData} />
           <BestSellerProducts />
           <OnSaleBanner data={secondBannerData} />
+          <RecentNews />
         </>
       )}
     </div>

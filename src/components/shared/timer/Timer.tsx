c1963087b3d33e2128058aebeed031a94e70052a
AshkanZj <ashkanzojaji19@gmail.com>
2024-07-31 13:57:13 +0330

    feat: create PaymentFa component

---
+++ src/components/shared/timer/Timer.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+type TimerPropsType = {
+  time: number;
+  action: () => void;
+};
+
+const Timer = ({ time, action }: TimerPropsType) => {
+  const [timeLeft, setTimeLeft] = useState(time);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(timer);
+          action();
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [action]);
+
+  return (
+    <>
+      <p>
+        {Math.floor(timeLeft / 60)
+          .toString()
+          .padStart(2, '0')}
+        :{(timeLeft % 60).toString().padStart(2, '0')}
+      </p>
+    </>
+  );
+};
+
+export default Timer;

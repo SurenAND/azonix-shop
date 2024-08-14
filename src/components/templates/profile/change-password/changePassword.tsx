6fc1f4b1ea524f8ee167c1e50da306d444cbaec5
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-08-14 15:23:13 +0330

    fix: profile ui fix in mobile view

--- src/components/templates/profile/change-password/changePassword.tsx
+++ src/components/templates/profile/change-password/changePassword.tsx
@@ -42,14 +42,16 @@ const ChangePassword = () => {
   };
 
   return (
-    <div className='flex h-[75vh] w-full flex-col items-center space-y-16 p-10'>
-      <div className='bg-profileGradient flex w-full gap-8 rounded-lg p-5'>
-        <div className='bg-profileGradient flex h-24 w-40 items-center justify-center rounded-full p-2 text-blue-500'>
+    <div className='flex min-h-[75vh] w-full flex-col items-center space-y-8 p-4 sm:p-10'>
+      <div className='flex w-full flex-col gap-4 rounded-lg bg-profileGradient p-4 sm:flex-row sm:gap-8 sm:p-5'>
+        <div className='flex h-16 w-16 items-center justify-center rounded-full bg-profileGradient p-2 text-blue-500 sm:h-24 sm:w-40'>
           <BsFillShieldLockFill className='h-2/3 w-2/3' />
         </div>
         <div className='flex flex-col'>
-          <h1 className='mt-2 text-3xl font-black'>{t('change-password')}</h1>
-          <p>{t('change-password-desc')}</p>
+          <h1 className='text-2xl font-black sm:text-3xl'>
+            {t('change-password')}
+          </h1>
+          <p className='text-sm sm:text-base'>{t('change-password-desc')}</p>
         </div>
       </div>
 
@@ -57,8 +59,8 @@ const ChangePassword = () => {
         onSubmit={handleSubmit(onSubmit)}
         className='flex w-full flex-col items-center gap-4'
       >
-        <div className='flex w-full flex-wrap justify-between p-5'>
-          <div className='m-1 w-full md:w-[45%]'>
+        <div className='flex w-full flex-col p-2 sm:flex-row sm:flex-wrap sm:justify-between sm:p-5'>
+          <div className='m-1 w-full sm:w-[45%]'>
             <label
               htmlFor='new-pass'
               className='mb-2 block text-sm font-medium text-gray-700'
@@ -74,7 +76,7 @@ const ChangePassword = () => {
             />
           </div>
 
-          <div className='m-1 w-full md:w-[45%]'>
+          <div className='m-1 w-full sm:w-[45%]'>
             <label
               htmlFor='confirm-new-pass'
               className='mb-2 block text-sm font-medium text-gray-700'
@@ -94,7 +96,7 @@ const ChangePassword = () => {
 
         <button
           type='submit'
-          className='rounded bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600'
+          className='w-full rounded bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600 sm:w-auto'
         >
           {t('save-changes')}
         </button>
